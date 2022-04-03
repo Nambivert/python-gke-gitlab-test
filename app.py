6a7ec91556e85b5c66ef9b6448075c42d5aa8f30
Nambi Srinivasan <79801348+Nambivert@users.noreply.github.com>
2022-04-03 14:41:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)   #app instance
 #root of application 
 @app.route("/")       #app as a decorator to create each router or URL provided by application 
 def hello(): 
-    return "hello, hi Pratishtha" 
+    return "hello, hi narayanan" 
  
 if __name__ == "__main__": 
     app.run()
